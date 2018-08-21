24550664a7b7fb55d09af094dea7ceeaa534cb8f
manojkmishra <manojmishra571980@gmail.com>
2018-08-21 15:37:28 +1000

    jwt auth working

--- src/modules/customer/customer.controller.js
+++ src/modules/customer/customer.controller.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import { PROVIDER_ENUM } from './customer.model';
 import { AuthProvider } from '../../services/authProvider';
 import { getOrCreateCustomer, me } from './customer';
+import { AuthServices } from '../../services/Auth';
 
 export const create = async (req, res) => 
 {  const { token, provider } = req.body;
@@ -21,7 +22,10 @@ export const create = async (req, res) =>
         else {  res.sendStatus(400);  }
         console.log('/cust.controller-data=',data);
         const customer = await getOrCreateCustomer(data, provider);
+        const jwtToken = AuthServices.createToken(customer);
+        res.status(200).json({ token: jwtToken });
         res.status(200).json(customer);
+        
     } 
   catch (error) {   res.status(400).json({ message: error.message });
                 }
--- src/modules/customer/customer.routes.js
+++ src/modules/customer/customer.routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { create} from './customer.controller';
+import { customerAuth } from './customer';
 
 const routes = Router();
 
 routes.post('/', create);
+routes.get('/hello',customerAuth,(req,res)=>{
+    console.log('/customer.route/hello--req.user=',req.user)
+    res.send('this is hello route');
+})
 
 export default routes;
